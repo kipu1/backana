09f614a1ad66d033e5f994d82ccbb6b68fcd5f92
Kevin Ortiz <111302391+kipu1@users.noreply.github.com>
2024-09-21 17:55:12 -0500

    security

--- src/main/java/com/example/Nutriologa/Analia/Roman/security/SecurityConfig.java
+++ src/main/java/com/example/Nutriologa/Analia/Roman/security/SecurityConfig.java
@@ -82,10 +82,9 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-
-        configuration.addAllowedOrigin("https://analiaromannutricionista.netlify.app");  // Permitir el frontend en Netlify// Permitir el frontend en Angular
-        configuration.addAllowedMethod("*");  // Permitir todos los métodos HTTP
-        configuration.addAllowedHeader("*");  // Permitir todos los encabezados
+        configuration.setAllowedOrigins(List.of("https://analiaromannutricionista.netlify.app"));  // Permitir el frontend en Netlify
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));  // Permitir todos los métodos HTTP
+        configuration.setAllowedHeaders(List.of("*"));  // Permitir todos los encabezados
         configuration.setAllowCredentials(true);  // Permitir el envío de credenciales como tokens o cookies
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
